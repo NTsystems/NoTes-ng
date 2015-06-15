5d637ccc02fa959a76aa17e6708479e3c5c84f88
sweetbee <chessogi@yahoo.com>
2015-06-15 17:07:47 +0200

    Updated bowerrc.

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,67 @@
-// include gulp
-var gulp = require('gulp');
-var bower = require('gulp-bower');
-var gulpFilter = require('gulp-filter');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+// dependencies
+var gulp = require('gulp'),
+    bower = require('gulp-bower'),
+    filter = require('gulp-filter'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
 
-// define base folders
-var src = {
-  css: ['./src/assets/**/*.css'],
-  js: ['./src/assets/**/*.js'],
-  bower: ['bower.json', '.bowerrc']
-}
-// define default destination folder
-var dest = 'src/dist/';
-
-// concat bower *.js to `notesb-ng.js`
-// and bower *.css to `notesb-ng.css`
-gulp.task('bower', function() {
-  var jsFilter = gulpFilter('**/*.js'),
-      cssFilter = gulpFilter('**/*.css');
-  
-  return gulp.src(['./src/assets/lib/**/*.js'])
-    .pipe(concat('notesb-ng.js'))
-    .pipe(gulp.dest('./src/dist/'));
-  // return bower('./src/assets/lib')
-  //   .pipe(jsFilter)
-  //   .pipe(concat('notesb-ng.js'))
-  //   .pipe(gulp.dest(dest))
-  //   .pipe(jsFilter.restore())   
-  //   .pipe(gulp.dest(dest))
-  //   .pipe(cssFilter)
-  //   .pipe(concat('notesb-ng.css'))
-  //   .pipe(gulp.dest(dest))
-  //   .pipe(cssFilter.restore())
+/**
+ * Installs app dependencies.
+ */
+gulp.task('bower', function () {
+  return bower({ cmd: 'update' });
 });
 
-// if we want to minify it, just add .pipe(rename({suffix: '.min'}))
-gulp.task('compress', function() {
-  return gulp.src(src.js)
-    .pipe(concat('notesa-ng.js'))
+/**
+ * Concatenates scripts.
+ */
+gulp.task('concatenate', function () {
+  return gulp.src(['./assets/libs/**/*.js', './src/**/*.js'])
+    .pipe(concat('notes.js'))
+    .pipe(gulp.dest('./dist'));
+});
+
+/**
+ * "Uglifies" concatenated files.
+ */
+gulp.task('compress', function () {
+  // TODO
+  return gulp.src('./dist/notes.js')
+    .pipe(concat('notes.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest(dest));
 });
 
- 
-// default task
-gulp.task('default', ['bower', 'compress']);
+// concat bower *.js to `notesb-ng.js`
+// and bower *.css to `notesb-ng.css`
+// gulp.task('bower', function() {
+//   var jsFilter = gulpFilter('**/*.js'),
+//       cssFilter = gulpFilter('**/*.css');
+  
+//   return gulp.src(['./src/assets/lib/**/*.js', './src/app'])
+//     .pipe(concat('notesb-ng.js'))
+//     .pipe(gulp.dest('./src/dist/'));
+//   // return bower('./src/assets/lib')
+//   //   .pipe(jsFilter)
+//   //   .pipe(concat('notesb-ng.js'))
+//   //   .pipe(gulp.dest(dest))
+//   //   .pipe(jsFilter.restore())   
+//   //   .pipe(gulp.dest(dest))
+//   //   .pipe(cssFilter)
+//   //   .pipe(concat('notesb-ng.css'))
+//   //   .pipe(gulp.dest(dest))
+//   //   .pipe(cssFilter.restore())
+// });
+
+// if we want to minify it, just add .pipe(rename({suffix: '.min'}))
+// gulp.task('compress', function() {
+//   return gulp.src(src.js)
+//     .pipe(concat('notesa-ng.js'))
+//     .pipe(uglify())
+//     .pipe(gulp.dest(dest));
+// });
+
+// tasks
+gulp.task('default', ['bower', 'concatenate']);
+gulp.task('deploy', ['default', 'compress']);
