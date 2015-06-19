f6d21ff5ee6094edac019d95b07576503d62a4a5
nemanja <nemanjakovach@gmail.com>
2015-06-19 10:49:55 +0200

    Delete mistakes

--- src/app.config.js.LOCAL.3272.js
+++
@@ -1,8 +0,0 @@
-// TO-DO
-var app = angular.module('app', ['ngRoute']).confug(function($routeProvider) {
-	$routeProvider.when('/notes', {
-		temlateUrl: 'src/dashboard/notes.html',
-		controller: ''
-	})
-	// body...
-})
